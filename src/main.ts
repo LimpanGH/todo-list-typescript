dfca2adb5692279a3198ef20b8418548a9f07601
LimpanGH <Linusj555@gmail.com>
2024-02-07 17:53:46 +0100

    creating to-dos and saving to ls now working

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,59 @@
-interface Todo {
-  title: string
+import { v4 as uuidv4 } from 'uuid';
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+console.log('hello');
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem)
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  input.value = '';
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    console.log(tasks);
+    saveTasks()
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
 }
 
-console.log('hello')
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks) )
+}
 
-import { a } from './test.js'
-
-a()
-
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS')
+  if (taskJSON == null ) return []
+  return JSON.parse(taskJSON)
+}
