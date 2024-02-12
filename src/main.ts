f8411ebc639b86b21850f1490257104fc5f1873d
LimpanGH <Linusj555@gmail.com>
2024-02-12 16:15:27 +0100

    started from scratch with imperatively declararing my html with js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
+console.log(uuidV4());
 
-// Skapar en typ som heter Task och typar dess properties med typescript-typer.
+// ----------------------------------------------------
 type Task = {
   id: string;
   title: string;
@@ -8,57 +9,67 @@ type Task = {
   createdAt: Date;
 };
 
-const list = document.querySelector<HTMLDivElement>('.notes-section');
+// ----------------------------------------------------
+const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
-const tasks: Task[] = loadTasks(); // loadTasks will return an arryay of tasks, each task will have the the type "Task", this array of tasks will be stored in the variable called "task".
-tasks.forEach(addListItem); // Loops through the array and runs the function "addListItem" on each item.
 
-// "form" could be null, if no task has been created, thats why I need the ?.
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+// console.log(tasks[0].completed);
+// ----------------------------------------------------
 form?.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Checks if the value property of the input object is null or an empty string, if it is, it stops the function.
   if (input?.value == '' || input?.value == null) {
     return;
   }
 
-  // Creating a new "Task" object and adds it to the array stored in the variable "tasks".
   const newTask: Task = {
-    id: uuidv4(),
+    id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
   };
   tasks.push(newTask);
+  saveTasks();
 
-  addListItem(newTask); // Function-call for "addListItem", passing the "newTask"-object as an argument.
-
-  input.value = ''; // Clearing the inputfield so I can input another task in the input-field.
+  addListItem(newTask);
+  input.value = '';
 });
 
+// ----------------------------------------------------
 function addListItem(task: Task) {
-  const checkbox = document.querySelector<HTMLInputElement>('.checkbox');
-  const notesSection = document.querySelector<HTMLDivElement>('.notes-section');
+  const item = document.createElement('li');
+  const label = document.createElement('label');
 
-  checkbox?.addEventListener('change', () => {
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X';
+  deleteBtn.classList.add('delete-btn');
+
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
     task.completed = checkbox.checked;
-    console.log(tasks);
     saveTasks();
   });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
 
-  if (checkbox !== null && checkbox !== undefined) {
-    checkbox.checked = task.completed;
-  }
-  notesSection.append(checkbox, document.createTextNode(task.title));
+  label.append(checkbox, task.title, deleteBtn);
+  item.append(label);
+  list?.append(item);
 }
 
 function saveTasks() {
   localStorage.setItem('TASKS', JSON.stringify(tasks));
 }
 
-function loadTasks(): Task[] {
-  const taskJSON = localStorage.getItem('TASKS');
-  if (taskJSON == null) return [];
-  return JSON.parse(taskJSON);
+function loadTasks() {
+  const tasksJSON = localStorage.getItem('TASKS');
+
+  if (tasksJSON == null) {
+    return [];
+  } else {
+    return JSON.parse(tasksJSON);
+  }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,39 @@
-export let a = function () {
-console.log('funkar?');
-}
+// //import { v4 as uuidv4 } from 'uuid'; Du kan använda denna om du vill ha en unik id för varje todo. Eller datum eller något annat.
+// // Definera egen typ för en todo
+// type Todo = {
+//     id: string,
+//     text: string,
+//     completed: boolean
+// }
+// // Global variabel för att lagra alla todos
+// const todos = []
+// // Läs in listan på sidin
+// function renderList(todos: Todo[]) {
+// // Använd map för att skapa en lista med alla todos
+// const html = todos.map(todo => {
+//     return `
+//         <li class="todo" data-id=${todo.id}>${todo.text}</li>
+//     `
+// })
+// }
+// // Lägga till en todo
+// function addTodo() {
+//     const newTodo: Todo = {
+//         id: uuidv4(), // uuid eller eller Date() eller annat
+//         text: "hej", // Hämta från input
+//         completed: false,
+//     }
+//     todos.push(newTodo)
+// }
+// // Uppdatera en todo (VG)
+// function updatedTodo(id: string, text: string) {
+//     // Filtera ut den todo som ska uppdateras
+//     updatedTodo = todos.filter(todo => id === todo.id)
+//     // Använd spread operator ... 
+//     const t = {...updatedTodo, text: "new text"}
+// }
+// // Ta bort en todo 
+// function removeTodo(id: string) {
+//     // Filtera ut den todo som ska tas bort
+//     const removeTodo = todos.filter( todo => id !== todo.id)
+// // }
